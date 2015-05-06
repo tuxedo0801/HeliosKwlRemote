989307330379a267621c28907d7c1be005b7992c
tuxedo0801 <alex@root1.de>
2015-05-06 14:24:30 +0200

    added better log-handling by using log.properties

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 JAVA=`which java`
 echo "Using Java: $JAVA"
-$JAVA -jar HeliosKwlRemote-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+$JAVA -Djava.util.logging.config.file=log.properties -jar HeliosKwlRemote-1.0.0-SNAPSHOT-jar-with-dependencies.jar 
